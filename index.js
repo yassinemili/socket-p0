960a4ebe1b0a4cee7bd7c6d6cb9cd861c7355ec2
Yassine Mili <ya.mili@icloud.com>
2025-04-24 00:24:45 +0100

    fix origins

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["https://elec-frontend.vercel.app"], // allow Vercel frontend
+        origin: ["https://elec-frontend.vercel.app", "https://elec-backend.vercel.app/"], // allow Vercel frontend
         methods: ["GET", "POST"],
         credentials: true
     }
